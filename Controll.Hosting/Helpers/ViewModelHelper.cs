917d710c129fc3fd0a2c55f6abbd6d65b804f8c3
thecopy <thecopy@gmail.com>
2013-03-11 20:02:36 +0100

    Moved sample projects away from this solution. Refactored ViewModel, they no longer depend on Controll.Hosting. Moved Activity and Zombie classes into Controll.Hosting.Models

---
+++ Controll.Hosting/Helpers/ViewModelHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Controll.Common;
+using Controll.Common.ViewModels;
+
+namespace Controll.Hosting.Helpers
+{
+    public static class ViewModelHelper
+    {
+        public static ActivityCommandViewModel Create(ActivityCommand command)
+        {
+            return new ActivityCommandViewModel().Fill(command);
+        }
+
+        public static ActivityCommandViewModel Fill(this ActivityCommandViewModel self, ActivityCommand command)
+        {
+            self.Name = command.Name;
+            self.Label = command.Label;
+            self.IsQuickCommand = command.IsQuickCommand;
+            self.ParameterDescriptors =
+                command.ParameterDescriptors.Select(pd => new ParameterDescriptorViewModel(pd)).ToList();
+
+            return self;
+        }
+
+        public static ActivityViewModel Fill(this ActivityViewModel self, Activity activity)
+        {
+            self.Key = activity.Id;
+            self.Name = activity.Name;
+            self.CreatorName = activity.CreatorName;
+            self.Version = activity.Version;
+            self.Description = activity.Description;
+            self.LastUpdated = activity.LastUpdated;
+            self.Commands = activity.Commands.Select(c => ViewModelExtensions.Create(c));
+
+            return self;
+        }
+    }
+}
